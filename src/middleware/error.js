bbc6e9d18910f2630e74aeef70d8ea7a17adcdb5
Daniel <danieldotinc@gmail.com>
2019-03-16 23:18:10 +0330

    logging completed

--- src/middleware/error.js
+++ src/middleware/error.js
@@ -1,18 +1,7 @@
 const winston = require("winston");
-const logger = winston.createLogger({
-  level: "error",
-  format: winston.format.json(),
-  defaultMeta: { service: "user-service" },
-  transports: [
-    new winston.transports.File({ filename: "error.log", level: "error" }),
-    new winston.transports.File({ filename: "combined.log" })
-  ]
-});
 
 module.exports = function(err, req, res, next) {
-  //Logging the error
-  logger.error(err.message, err);
+  winston.error(err.message, err);
   //error, warn, info, verbose, debug, silly
-
   res.status(500).send("Something Failed.");
 };
--- src/routes/api.js
+++ src/routes/api.js
@@ -1,30 +1,11 @@
-require("express-async-errors");
-const error = require("../middleware/error");
 const express = require("express");
-const mongoose = require("mongoose");
-const products = require("./products");
-const users = require("./users");
-const auth = require("./auth");
-const config = require("config");
-
+const winston = require("winston");
 const app = express();
 
-if (!config.get("jwtPrivateKey")) {
-  console.log("Fatal Error: jwtPrivateKey is not defined");
-  process.exit(1);
-}
+require("../startup/logging");
+require("../startup/config")();
+require("../startup/db")();
+require("../startup/routes")(app);
 
-mongoose
-  .connect("mongodb://localhost/store-manager")
-  .then(() => console.log("Connected to MongoDB ..."))
-  .catch(err => console.error("Could not connect to MongoDB ..."));
-
-app.use(express.json());
-
-app.use("/api/products", products);
-app.use("/api/users", users);
-app.use("/api/auth", auth);
-
-app.use(error);
-
-app.listen(3000);
+const port = process.env.PORT || 3000;
+app.listen(port, winston.info(`Listening on port ${port} ...`));
